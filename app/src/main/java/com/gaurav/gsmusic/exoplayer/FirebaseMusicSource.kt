98e12149a06e99aad5c71989c43e0eb3eaf3ea6d
Gaurav Singh <gauravs21k@gmail.com>
2022-03-23 02:00:07 +0530

    FirebaseMusicSource.kt created and whenReady and state functions added to check state of songs

---
+++ app/src/main/java/com/gaurav/gsmusic/exoplayer/FirebaseMusicSource.kt
@@ -0,0 +1,39 @@
+package com.gaurav.gsmusic.exoplayer
+
+import com.gaurav.gsmusic.exoplayer.State.*
+
+class FirebaseMusicSource {
+
+    private val onReadyListener = mutableListOf<(Boolean) -> Unit>()
+
+    private var state: State = STATE_CREATED
+        set(value) {
+            if (value == STATE_INITIALIZED || value == STATE_ERROR) {
+                synchronized(onReadyListener) {
+                    field = value
+                    onReadyListener.forEach { listener ->
+                        listener(state == STATE_INITIALIZED)
+                    }
+                }
+            } else {
+                field = value
+            }
+        }
+
+    fun whenReady(action: (Boolean) -> Unit): Boolean {
+        return if (state == STATE_CREATED || state == STATE_INITIALIZING) {
+            onReadyListener += action
+            false
+        } else {
+            action(state == STATE_INITIALIZED)
+            true
+        }
+    }
+}
+
+enum class State {
+    STATE_CREATED,
+    STATE_INITIALIZING,
+    STATE_INITIALIZED,
+    STATE_ERROR
+}
